fc5618b089766edb91d82dbd356c508e66c01628
Jason Ginsberg <ginsdaman@gmail.com>
2014-05-03 16:54:37 -0400

    commit for deploy to heroku

--- music/views.py
+++ music/views.py
@@ -22,7 +22,7 @@ def song_view(request):
 	month = int(request.GET['month'])
 	day = int(request.GET['day'])
 	date = datetime(year,month,day)
-	topSong=list(TopSong.objects.filter(date__gte=date).order_by('date').limit(1))
+	topSong=list(TopSong.objects.filter(date__gte=date).order_by('date')[:1])
 	if topSong:
 		return HttpResponse(dumps(model_to_dict(topSong[0],exclude=['id']), cls=DjangoJSONEncoder))
 	else:
